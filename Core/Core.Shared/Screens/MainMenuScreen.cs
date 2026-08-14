f86be3075a9545a8c2427d06e4c3b9409ded5c5d
Morgan <murgntheurgn@gmail.com>
2026-08-14 16:51:42 +0100

    Oops missed one

--- Core/Core.Shared/Screens/MainMenuScreen.cs
+++ Core/Core.Shared/Screens/MainMenuScreen.cs
@@ -1,27 +1,17 @@
 using Engine.Debugging;
 using Engine.Screens;
 using Microsoft.Xna.Framework;
-using MonoGameGum;
 
 namespace Core.Scenes
 {
     public class MainMenuScreen : GameObjectScreen
     {
-        
         public MainMenuScreen(Game game) : base(game, "MainMenuScreen") { }
 
         public override void Initialize()
         {
             base.Initialize();
             CameraScript.ClearColor = Color.Black;
-            
-            // canvas = new MainMenuCanvas(Game, this);
-            // canvas.Initialize();
-
-            var ui = new MainMenuUIRuntime();
-            ui.game = Game;
-            ui.AddToRoot();
-            
         }
 
         public override void Update(GameTime gameTime)
